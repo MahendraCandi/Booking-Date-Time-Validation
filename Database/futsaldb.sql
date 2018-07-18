6a7e8fc0f703429dc04121a279953f3c77c302cc
MahendraCandi <abdumaadjidcandi@gmail.com>
2018-07-19 06:46:49 +0700

    Database update

--- Database/futsaldb.sql
+++ Database/futsaldb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 12, 2018 at 12:50 AM
+-- Generation Time: Jul 19, 2018 at 01:45 AM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -32,6 +32,13 @@ CREATE TABLE IF NOT EXISTS `akun` (
   `jenis_akun` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `akun`
+--
+
+INSERT INTO `akun` (`kd_akun`, `nm_akun`, `jenis_akun`) VALUES
+('Akun-001', 'Bayar Listrik', 'Aktiva');
+
 -- --------------------------------------------------------
 
 --
@@ -46,10 +53,23 @@ CREATE TABLE IF NOT EXISTS `booking` (
   `jam_masuk` time NOT NULL,
   `jam_keluar` time NOT NULL,
   `diskon` double NOT NULL,
+  `hari_libur` double NOT NULL,
   `kd_lap` varchar(7) NOT NULL,
   `uang_dp` double NOT NULL,
   `kd_user` varchar(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `booking`
+--
+
+INSERT INTO `booking` (`kd_booking`, `tgl_booking`, `kd_pelanggan`, `tgl_pakai`, `jam_masuk`, `jam_keluar`, `diskon`, `hari_libur`, `kd_lap`, `uang_dp`, `kd_user`) VALUES
+('Book-001', '2018-07-13', 'Team-001', '2018-07-14', '17:00:00', '19:00:00', 0, 0, 'Lap-001', 50000, 'User-001'),
+('Book-002', '2018-07-13', 'Team-002', '2018-07-14', '15:00:00', '16:00:00', 10, 0, 'Lap-002', 50000, 'User-001'),
+('Book-003', '2018-07-14', 'Team-002', '2018-07-15', '14:00:00', '16:00:00', 0, 0, 'Lap-001', 50000, 'User-001'),
+('Book-004', '2018-07-15', 'Team-002', '2018-07-16', '15:00:00', '17:00:00', 0, 0, 'Lap-001', 100000, 'User-001'),
+('Book-005', '2018-07-15', 'Team-002', '2018-07-17', '16:00:00', '18:00:00', 0, 0, 'Lap-001', 50000, 'User-001'),
+('Book-006', '2018-07-17', 'Team-002', '2018-07-17', '06:00:00', '08:00:00', 18000, 0, 'Lap-001', 50000, 'User-001');
 
 -- --------------------------------------------------------
 
@@ -89,6 +109,18 @@ CREATE TABLE IF NOT EXISTS `lapangan` (
   `tarif` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `lapangan`
+--
+
+INSERT INTO `lapangan` (`kd_lap`, `jenis_lap`, `tarif`) VALUES
+('Lap-001', 'Vinyl', 90000),
+('Lap-002', 'Vinyl', 90000),
+('Lap-003', 'Vinyl', 90000),
+('Lap-004', 'Rumput', 80000),
+('Lap-005', 'Rumput', 80000),
+('Lap-006', 'Rumput', 80000);
+
 -- --------------------------------------------------------
 
 --
@@ -101,6 +133,14 @@ CREATE TABLE IF NOT EXISTS `pelanggan` (
   `alamat` varchar(100) NOT NULL,
   `no_hp` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pelanggan`
+--
+
+INSERT INTO `pelanggan` (`kd_pelanggan`, `nm_pelanggan`, `alamat`, `no_hp`) VALUES
+('Team-001', 'The Report', 'Jalan Hj. Gedad No 78 Ciledug Karang Tengah', '081928771877'),
+('Team-002', 'The Ice Holes', 'Jalan Raya No. 789 Kecamatan No 39', '08991119991991');
 
 -- --------------------------------------------------------
 
@@ -117,11 +157,20 @@ CREATE TABLE IF NOT EXISTS `penyewaan` (
   `jam_sewa_masuk` time NOT NULL,
   `jam_sewa_keluar` time NOT NULL,
   `lama_sewa` double NOT NULL,
+  `hari_libur` double NOT NULL,
   `diskon_sewa` double NOT NULL,
   `total_sewa` double NOT NULL,
   `uang_byr` double NOT NULL,
   `kd_user` varchar(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `penyewaan`
+--
+
+INSERT INTO `penyewaan` (`no_trans`, `tgl_sewa`, `kd_pelanggan`, `kd_booking`, `kd_lap`, `jam_sewa_masuk`, `jam_sewa_keluar`, `lama_sewa`, `hari_libur`, `diskon_sewa`, `total_sewa`, `uang_byr`, `kd_user`) VALUES
+('Trans-001', '2018-07-14', 'Team-001', 'Book-001', 'Lap-001', '17:00:00', '19:00:00', 2, 0, 0, 285000, 300000, 'User-001'),
+('Trans-002', '2018-07-16', 'Team-002', 'Book-002', 'Lap-002', '15:00:00', '16:00:00', 1, 0, 0, 115000, 65000, 'User-001');
 
 -- --------------------------------------------------------
 
